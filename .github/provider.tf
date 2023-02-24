9586ff0a3cba4c2466ea681a1aba3b1a3ace6a7f
arvindannamalai <48624849+arvindannamalai@users.noreply.github.com>
2023-02-24 10:51:31 +0530

    Update provider.tf

--- .github/provider.tf
+++ .github/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIA6Q3QI5762WBDB45Q"
-  secret_key = "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"
+  access_key = "AKIAWRS4SCP66L2AGKDL"
+  secret_key = "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"
 }
