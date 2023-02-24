fde5e323501b1d78a5d513be21e61244463da66f
A <arvind.a.a@capgemini.com>
2023-02-24 10:34:16 +0530

    Test

---
+++ .github/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = "AKIA6Q3QI5762WBDB45Q"
+  secret_key = "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"
+}
---
+++ .github/variable.tf
@@ -0,0 +1,4 @@
+variable "acme_s3_logging_bucket" {
+  description = "The s3 logging bucket"
+  default = "test-bucket"
+}
